c1846d4ad6744eeb7d7fd2f21d4e6d727e273caf
nitsuga123 <pepe_diaz92@hotmail.com>
2016-08-30 12:31:26 -0500

    examen

---
+++ Examen1AgustinDiazFernandez/Assets/scripts/Shoot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Shoot : MonoBehaviour {
+    [SerializeField]
+    private GameObject bala;
+
+    [SerializeField]
+    private GameObject origenBala;
+
+    [SerializeField]
+    private float fuerza;
+
+
+	// Use this for initialization
+	void Start () {
+ 
+        origenBala = new GameObject();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bala, origenBala.transform.position, Quaternion.identity);
+            bala.GetComponent<Rigidbody>().AddForce(transform.forward * fuerza*Time.deltaTime);
+
+        }
+	}
+
+    
+
+    public void disparar() {
+
+    }
+
+}
---
+++ Examen1AgustinDiazFernandez/Assets/scripts/enemigo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemigo : MonoBehaviour {
+
+   [SerializeField]
+   private int vida;
+
+    [SerializeField]
+    private int daño;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (vida <= 0) {
+            Destroy(this.gameObject);
+        }
+   
+	}
+
+    public void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.tag == "bala") {
+            vida -= daño;
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
